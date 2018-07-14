c2171158d01c41aa82480a4ac8c233e074ffbcaf
Scott Ungchusri <animoogle504@gmail.com>
2018-07-14 23:28:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -104,7 +104,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |pointer,items|
       if items.include?("BBQ")
-        returnArray << holiday.to_s
+        returnArray << pointer.to_s
       end
     end
   end
